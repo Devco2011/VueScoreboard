b1c33f3ff246a03771024993f8bdd8450ca259f3
Devco2011 <devco2011@gmail.com>
2021-03-05 12:05:14 -0600

    scoreboard working

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,92 @@
 <template>
   <div id="app">
-    <h1>Player 1 vs Player 2</h1>
-    <div class="scoreCards">
-      <div class="scoreCard">
-        <h2>0</h2>
-        <button class="decrement">-</button>
-        <button class="increment">+</button>
-      </div>
-    </div>
-    <div class="scoreCards">
-      <div class="scoreCard">
-        <h2>0</h2>
-        <button class="decrement">-</button>
-        <button class="increment">+</button>
-      </div>
+    <matchup-title :team1="team1" :team2="team2"></matchup-title>
+    <div class="box">
+      <scorecard
+        :score="team1.score"
+        @pointAdded="updateTeamScore(team1, 1)"
+        @pointSubtracted="updateTeamScore(team1, -1)"
+      ></scorecard>
+      <scorecard
+        :score="team2.score"
+        @pointAdded="updateTeamScore(team2, 1)"
+        @pointSubtracted="updateTeamScore(team2, -1)"
+      ></scorecard>
     </div>
   </div>
 </template>
 
 <script>
+import Scorecard from "./components/Scorecard.vue";
+import MatchupTitle from "./components/MatchupTitle.vue";
+
+export default {
+  components: { MatchupTitle, Scorecard },
+
+  data() {
+    return {
+      team1: {
+        name: "Player 1",
+        score: 0,
+      },
+      team2: {
+        name: "Player 2",
+        score: 0,
+      },
+    };
+  },
+
+  methods: {
+    updateTeamScore(team, points) {
+      const updated = team.score + points;
+
+      if (updated >= 0) {
+        team.score = updated;
+      }
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #03b8fa;
+}
+h1 {
+  text-align: center;
+  font-size: 5rem;
+}
+
+.box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.scoreCard {
+  border: 2px #f4f4f4 solid;
+  font-size: 4rem;
+  margin: 3rem;
+  padding: 1rem;
+}
+
+button {
+  width: 10vw;
+  height: 10vh;
+  font-size: 3rem;
+  color: #fff;
+  margin: 2rem;
+}
+
+.decrement {
+  background: rgb(255, 0, 0, 0.5);
+  border: 2px rgb(255, 0, 0) solid;
+}
+
+.increment {
+  background: rgb(0, 255, 0, 0.5);
+  border: 2px rgb(0, 255, 0) solid;
+}
 </style>
---
+++ src/components/MatchupTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>
+      <span :class="{ winning: team1.score > team2.score }">{{
+        team1.name
+      }}</span>
+      vs
+      <span :class="{ winning: team2.score > team1.score }"
+        >{{ team2.name }}
+      </span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["team1", "team2"],
+};
+</script>
+
+<style>
+.winning {
+  color: yellow;
+}
+</style>
